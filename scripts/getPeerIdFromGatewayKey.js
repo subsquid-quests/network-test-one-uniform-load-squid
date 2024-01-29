6d548edb3992fd09dc528eb9559744fd807dc8b3
abernatskiy <abernatskiy@toha.li>
2024-01-29 18:07:29 +0900

    modified:   scripts/getPeerIdFromGatewayKey.js

--- scripts/getPeerIdFromGatewayKey.js
+++ scripts/getPeerIdFromGatewayKey.js
@@ -7,7 +7,7 @@ const file = readFileSync(process.argv[2])
 async function main() {
 	const key =  await keys.supportedKeys.ed25519.unmarshalEd25519PrivateKey((file))
 	const peerId = await createFromPrivKey(key.bytes)
-	console.log(peerId.toB58String())
+	console.log('Your peer ID is', peerId.toB58String())
 }
 
 main()
